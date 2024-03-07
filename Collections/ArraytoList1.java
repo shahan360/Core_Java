f50906bbde3db26e1c47afd09e0cbe92239a3a52
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-03-06 21:10:08 -0600

    commited on 6th March 2024

---
+++ Collections/ArraytoList1.java
@@ -0,0 +1,18 @@
+import java.util.*; // Arrays, List collection classes are present in this package
+
+public class ArraytoList1 { // List is an interface and Arrays is its class
+    public static void main(String[] args) { //main method starts
+        //Sample Array
+        String[] array = {"mohit", "shashank", "akash", "deepak"}; //creating an array of strings
+
+        //Converting Array to List using Arrays.asList() method
+        List<String> listFromArray = new ArrayList<>(Arrays.asList(array)); //creating an instance of ArrayList and then creating a list from the array using Arrays.asList() method
+
+        //Printing the List resulted from Array
+        System.out.println("List from Array: " + listFromArray); //printing the list resulted from the array
+    }
+}
+
+
+/* In the above program, we have created an array of strings. Then we have created a list from the array using Arrays.asList() method. 
+Then we have printed the list resulted from the array. The Arrays.asList() method is used to convert an array to a list. */
